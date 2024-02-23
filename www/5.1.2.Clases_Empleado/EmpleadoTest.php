e0891934dff1320ae396e241cb80dd76a40eb343
joserosalessueiro <jrsueiro@yahoo.es>
2024-02-23 18:54:51 +0100

    Tarea 5.1.2 Empleado terminado

---
+++ www/5.1.2.Clases_Empleado/EmpleadoTest.php
@@ -0,0 +1,18 @@
+<?php
+require_once('Empleado.php');
+
+$empleado1 = new Empleado('Michael',1500);
+echo ' Nombre: '. $empleado1->getNombre() . "<br>";
+echo ' Número: ' . Empleado::$numEmpleados ."<br>";
+echo $empleado1->getSalario()."<br><br>";
+
+$empleado2 = new Empleado('John',152);
+echo ' Nombre: '. $empleado2->getNombre() ."<br>";
+echo ' Número: ' . Empleado::$numEmpleados ."<br>";
+echo  $empleado2->getSalario()."<br><br>";
+
+$empleado3 = new Empleado('Sarah',985);
+echo ' Nombre: '. $empleado3->getNombre() . "<br>";
+echo ' Número: ' . Empleado::$numEmpleados ."<br>";
+echo $empleado3->getSalario()."<br><br>";
+?>
